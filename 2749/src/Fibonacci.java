ebdaeb16aa2d7a0416d932c514d9b8c388ea5984
dhsong <dhsong951030@gmail.com>
2018-08-09 10:48:30 +0900

    런타임 에러

---
+++ 2749/src/Fibonacci.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Fibonacci {
+	int divider;
+	long n;
+
+	public Fibonacci(long n){
+		this.divider = 1000000;
+		this.n = n;
+	}
+	
+	public int fib_mod(){
+		ArrayList<Integer> period_arr = pisano(divider);
+		
+		int m = (int)(n % period_arr.size());
+
+		return period_arr.get(m);
+	}   
+	private ArrayList<Integer> pisano(int d){
+		ArrayList<Integer> period_arr = new ArrayList<>();
+		int idx = 1;
+		
+		period_arr.add(0);
+		period_arr.add(1);
+
+		while(true){
+			int a = period_arr.get(idx);
+			int b = (period_arr.get(idx-1) + a) % d;
+			idx++;
+			if(a != 0 || b != 1){
+				period_arr.add(b);
+			}
+			else{
+				break;
+			}			
+		}
+		return period_arr;
+	} 
+}
---
+++ 2749/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    	long n = Integer.parseInt(br.readLine());
+    	Fibonacci fib = new Fibonacci(n);
+    	
+    	int res = fib.fib_mod();
+	
+		bw.write(String.valueOf(res));
+    	bw.flush();
+
+    	bw.close();
+    	br.close();
+    }
+}
